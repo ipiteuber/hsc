5298e966e2fc74d43c898dfeb6514d1e12aa2db4
ipiteuber <140553021+ipiteuber@users.noreply.github.com>
2025-04-28 12:38:16 -0400

    Correccion serializers

--- Hsc/urls.py
+++ Hsc/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Inicio.urls')),
+    path('api/',include('rest_api.urls')),
 ]
 
--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,22 +1,62 @@
 from rest_framework import serializers
-from Inicio.models import Usuario, Producto, Marca, Categoria
+from Inicio.models import TipoUsuario, Usuario, Comuna, Region, Direccion, Venta, Categoria, TipoProd, Marca, Modelo, Producto, Detalle
+
+class TipoUsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoUsuario
+        fields = '__all__'
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = '__all__'  # Trae todos los campos 
+        fields = '__all__'
 
-class ProductoSerializer(serializers.ModelSerializer):
+class ComunaSerializer(serializers.ModelSerializer):
     class Meta:
-        model= Producto
-        fields= '__all__'
+        model = Comuna
+        fields = '__all__'
 
-class MarcaSerializer(serializers.ModelSerializer):
+class RegionSerializer(serializers.ModelSerializer):
     class Meta:
-        model= Marca
-        fields= '__all__'
+        model = Region
+        fields = '__all__'
+
+class DireccionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Direccion
+        fields = '__all__'
+
+class VentaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venta
+        fields = '__all__'
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
+        fields = '__all__'
+
+class TipoProdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TipoProd
+        fields = '__all__'
+
+class MarcaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Marca
+        fields = '__all__'
+
+class ModeloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Modelo
+        fields = '__all__'
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = '__all__'
+
+class DetalleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Detalle
         fields = '__all__'
